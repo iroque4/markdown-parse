05ada1c60ba29ffcdda2b4de2bc2868d91930e5a
Ivana <iroque1008@gmail.com>
2022-02-25 02:29:14 -0800

    lab 4 report

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -18,4 +18,25 @@ public class MarkdownParseTest {
         assertEquals(List.of("https://something.com", "some-page.html")
         , MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
     }
+
+    @Test
+    public void testSnippet1() throws IOException{
+        String contents = Files.readString(Path.of("lab-4-snippet-1.md"));
+        List<String> expect = List.of("`google.com", "google.com", "ucsd.edu");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        String contents = Files.readString(Path.of("lab-4-snippet-2.md"));
+        List<String> expect = List.of("a.com", "a.com(())", "example.com");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        String contents = Files.readString(Path.of("lab-4-snippet-3.md"));
+        List<String> expect = List.of("https://ucsd-cse15l-w22.github.io/");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
 }
